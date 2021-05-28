fc29323134efe84e83b1b64f3c7fe7b5fe00b9ef
ZhongRuoyu <zhongruoyu@outlook.com>
2021-05-28 16:59:13 +0800

    Integral types corrected

--- error.c
+++ error.c
@@ -5,9 +5,9 @@
 
 void error(const SourceFile *source, size_t line, size_t column, const char *msg) {
     fprintf(stderr, "\n");
-    fprintf(stderr, "At Line %d, Column %d: error: %s\n", line + 1, column + 1, msg);
+    fprintf(stderr, "At Line %zu, Column %zu: error: %s\n", line + 1, column + 1, msg);
     fprintf(stderr, "    %s\n", source->lines[line]);
-    fprintf(stderr, "    %*s^\n", column, "");
+    fprintf(stderr, "    %*s^\n", (int)column, "");
     fprintf(stderr, "\n");
 }
 
